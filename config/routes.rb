87ac2d5d1e79c0030af20538858dfb9a95279fe8
midori <m.id152771@outlook.jp>
2019-10-13 01:24:00 +0000

    rails 02

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+end
+
+Rails.application.routes.draw do
+  get 'users/new'
+  root 'pages#index'
+  get 'pages/help'
+end
