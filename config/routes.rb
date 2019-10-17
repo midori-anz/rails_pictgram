59cbe8ded68f821d92030d568439d572dd4a542d
midori <m.id152771@outlook.jp>
2019-10-17 01:31:48 +0000

    rails03

--- config/routes.rb
+++ config/routes.rb
@@ -15,3 +15,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
 end
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+end
