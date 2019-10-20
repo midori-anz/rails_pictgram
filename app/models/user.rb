96e99e2a4d64a5cd223ee2378e748dc113bd53bb
midori <m.id152771@outlook.jp>
2019-10-20 02:00:41 +0000

    1020

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
- validates :name, presence: true
- validates :email, presence: true
+ # validates :name, presence: true,length:{ maximum: 15 }
+ # validates :email, presence: true, uniqueness: true,
+ # format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
  
- has_secure_password
+ # validates :password_digest, presence: true, uniqueness: true,
+ # format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
+ 
+ # has_secure_password
+  validates :name,presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/ }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
+  has_secure_password
 end
