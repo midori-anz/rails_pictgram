ea5b5fd0501d6cfd5af8c27c43819d1e6c28ee3a
midori <m.id152771@outlook.jp>
2019-10-28 03:02:03 +0000

    rails 06

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/ }
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
   has_secure_password
+  
+  has_many :topics
+   
 end
